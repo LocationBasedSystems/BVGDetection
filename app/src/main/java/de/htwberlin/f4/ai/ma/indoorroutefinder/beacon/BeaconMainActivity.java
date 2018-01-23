2401bb28f7cb6c43294a11e1dfeee109178b3462
Julius Vinnen <julius-vinnen@t-online.de>
2018-01-23 16:18:18 +0100

    Enable Buttons added

--- app/src/main/java/de/htwberlin/f4/ai/ma/indoorroutefinder/beacon/BeaconMainActivity.java
+++ app/src/main/java/de/htwberlin/f4/ai/ma/indoorroutefinder/beacon/BeaconMainActivity.java
@@ -2,10 +2,16 @@ package de.htwberlin.f4.ai.ma.indoorroutefinder.beacon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
+import android.widget.Switch;
 
 import de.htwberlin.f4.ai.ma.indoorroutefinder.R;
 import de.htwberlin.f4.ai.ma.indoorroutefinder.android.BaseActivity;
+import de.htwberlin.f4.ai.ma.indoorroutefinder.persistence.DatabaseHandler;
+import de.htwberlin.f4.ai.ma.indoorroutefinder.persistence.DatabaseHandlerFactory;
 
 public class BeaconMainActivity extends BaseActivity {
 
@@ -15,5 +21,41 @@ public class BeaconMainActivity extends BaseActivity {
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.activity_beacon_main, contentFrameLayout);
         setTitle("Beacon ");
+
+        final Button buttonSend = (Button)findViewById(R.id.beacon_send_data);
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //wifiConn.sendMessage(tmp);
+            }
+        });
+
+        final Button buttonReceive = (Button)findViewById(R.id.beacon_receive_data);
+        buttonReceive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Switch enableSwitch = (Switch) findViewById(R.id.beacon_enable);
+        enableSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    buttonSend.setEnabled(true);
+                    buttonReceive.setEnabled(true);
+                }
+                else{
+                    buttonSend.setEnabled(false);
+                    buttonReceive.setEnabled(false);
+                }
+            }
+        });
+        //DatabaseHandlerFactory.getInstance(this).exportDatabase();
     }
+
+
 }
